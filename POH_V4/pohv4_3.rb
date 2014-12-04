6b5dab8b8280597c2e15da0d2a7a3e1305b70928
Hiroyuki Tanaka <ht0919@gmail.com>
2014-12-04 18:03:28 +0900

    chg PHO_V4_3:Ruby

--- POH_V4/pohv4_3.rb
+++ POH_V4/pohv4_3.rb
@@ -1,13 +1,21 @@
-max = 0
-m = Array.new(300000)
 s = gets.chomp.split(" ")
 t = s[0].to_i
 n = s[1].to_i
-n.times { |i| m[i] = gets.to_i }
-l = n - (t-1)
-l.times { |i|
-    sum = 0
-    t.times { |j| sum += m[i+j] }
+m = Array.new(n)
+sum = 0
+i = 0
+t.times {
+    m[i] = gets.to_i
+    sum += m[i]
+    i += 1
+}
+max = sum
+l = n - i
+l.times {
+    m[i] = gets.to_i
+    sum += m[i]
+    sum -= m[i-t]
     if sum>max then max=sum end
+    i += 1
 }
 puts max
