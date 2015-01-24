34312733e01b2c31cabbb04ff98cfb08b7e33e1d
Hiroyuki Tanaka <ht0919@gmail.com>
2015-01-24 10:05:04 +0900

    chg d002.php

--- D/d002.php
+++ D/d002.php
@@ -1,18 +1,12 @@
 <?php
-$s = trim(fgets(STDIN));
-$s = str_replace(array("\r\n","\r","\n"), '', $s);
-$s = explode(" ", $s);
-$a = intval($s[0]);
-$b = intval($s[1]);
-
-if ($a == $b) {
-    echo "eq";
-} else {
-    if ($a > $b) {
-        echo $a;
-    } else {
-        echo $b;
-    }
-}
-echo "\n";
+  $s = trim(fgets(STDIN));
+  $s = str_replace(array("\r\n","\r","\n"), '', $s);
+  $s = explode(" ", $s);
+  if ($s[0] > $s[1]) {
+    echo $s[0]."\n";
+  } else if ($s[0] < $s[1]) {
+    echo $s[1]."\n";
+  } else {
+    echo "eq\n";
+  }
 ?>
