e83ed3a989d6d1287d8b9167ad730abb4e518c2c
Hiroyuki Tanaka <ht0919@gmail.com>
2015-01-31 14:33:34 +0900

    chg d003.php

--- D/d003.php
+++ D/d003.php
@@ -1,10 +1,7 @@
 <?php
-$s = trim(fgets(STDIN));
-$s = str_replace(array("\r\n","\r","\n"), '', $s);
-$s = intval($s);
-
+$n = trim(fgets(STDIN));
 for ($i=1; $i<10; $i++) {
-    echo $s*$i;
+    echo $i * $n;
     if ($i<9) echo " ";
     else      echo "\n";
 }
