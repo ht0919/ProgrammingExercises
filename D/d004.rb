485d2979493f1f4ee5464b3d5afc32a604b6f29f
Hiroyuki Tanaka <ht0919@gmail.com>
2015-02-04 15:18:14 +0900

    chg D004:Ruby

--- D/d004.rb
+++ D/d004.rb
@@ -3,9 +3,9 @@ print "Hello "
 n.times do |i|
     s = gets.chomp
     print s
-    if (i+1) == n then  # 最後の単語なら "."と改行
-        puts "."
-    else                # そうでなければ ","
-        print ","
+    if (i+1) == n then
+        puts "."  # 最後の単語なら"."と改行
+    else
+        print "," # そうでなければ","
     end
 end
