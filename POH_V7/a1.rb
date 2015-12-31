f9aada71f421885f44978e89c13bbfa81cf60c27
Hiroyuki Tanaka <ht0919@gmail.com>
2016-01-01 02:04:55 +0900

    Chg a1.rb

--- POH_V7/a1.rb
+++ POH_V7/a1.rb
@@ -1,17 +1,20 @@
+def fact(n)
+  m = 1
+  for x in 2 .. n
+    m *= x
+  end
+  m
+end
 n = gets.to_i
-print "0 ",Time.now.strftime("%H:%M:%S"),"\n"
-m = (1..n).inject(:*)
-print "1 ",Time.now.strftime("%H:%M:%S"),"\n"
-t1 = m.to_s
-print "2 ",Time.now.strftime("%H:%M:%S"),"\n"
+  print "0 ",Time.now.strftime("%H:%M:%S"),"\n"
+#t1 = (1..n).inject(:*).to_s
+#t1 = (1..n).reduce(&:*).to_s
+t1 = fact(n).to_s
+  print "1 ",Time.now.strftime("%H:%M:%S"),"\n"
 p2 = t1.rindex(/[^0]/)
-print "3 ",Time.now.strftime("%H:%M:%S"),"\n"
 p1 = p2 - 8
 p1 = 0 if p1<0
 t2 = t1.slice(p1..p2)
-print "4 ",Time.now.strftime("%H:%M:%S"),"\n"
 p1 = t2.index(/[^0]/)
-print "5 ",Time.now.strftime("%H:%M:%S"),"\n"
 ans = t2.slice(p1,t2.length-p1)
-print "6 ",Time.now.strftime("%H:%M:%S"),"\n"
 puts ans
