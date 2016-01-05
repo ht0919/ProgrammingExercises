9c28696bbdedf6dc4bafc83f7917f2ddb5eebeaa
Hiroyuki Tanaka <ht0919@gmail.com>
2016-01-05 09:54:07 +0900

    Revert "Chg b1.rb"
    This reverts commit 3ff6fe67cddfd14d8fafb77efa962f66956372be.

--- POH_V7/b1.rb
+++ POH_V7/b1.rb
@@ -1,4 +1,28 @@
-x,y,z,n=gets.split.map(&:to_i)
-t=[[0,x],[0,y]]
-n.times{a,b=gets.split.map(&:to_i);t[a]<< b}
-p z*t.map{|e|e.sort.each_cons(2).map{|x,y|y-x}.min}.reduce(:*)
+q = Array.new
+n = gets.to_i
+n.times{
+  q << gets.chomp.split(" ")
+}
+
+p = Array.new
+m = gets.to_i
+m.times{
+  p << gets.chomp.split(" ")
+}
+
+s = q.size - p.size + 1
+t = false
+s.times {|i|
+  s.times {|j|
+    r = Array.new
+    (p.size).times {|k|
+      r << q[i+k].slice(j,p.size)
+    }
+    if r == p
+      puts "#{i} #{j}"
+      t = true
+      break
+    end
+  }
+  break if t == true
+}
